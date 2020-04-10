e9846934286853c30cc1df419c3afc79fa23f40f
Analuizasilva <a.silva@recodme.pt>
2020-04-10 21:48:44 +0100

    Finalizado

--- JogoDaForca/JogoDaForcaSolucao.cs
+++ JogoDaForca/JogoDaForcaSolucao.cs
@@ -9,8 +9,8 @@ namespace JogoDaForca
         Console.WriteLine("  ________     ");
         Console.WriteLine(" |        |         ");
         Console.WriteLine(" |        O         ");
-        Console.WriteLine(" |       /|     ");
-        Console.WriteLine(" |       /      "); //ja tem o resto dos membros ^_^
+        Console.WriteLine(" |       /|\\     ");
+        Console.WriteLine(" |       / \\     "); //ja tem o resto dos membros ^_^
         Console.WriteLine("");
         Console.WriteLine("");
         Console.WriteLine("");
