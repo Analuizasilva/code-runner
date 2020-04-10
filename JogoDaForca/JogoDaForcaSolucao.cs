9dbe21ead80a1093ea1ec47726eadd2771dce85b
Analuizasilva <a.silva@recodme.pt>
2020-04-10 21:52:24 +0100

    Falta finalizar.

--- JogoDaForca/JogoDaForcaSolucao.cs
+++ JogoDaForca/JogoDaForcaSolucao.cs
@@ -10,7 +10,7 @@ namespace JogoDaForca
         Console.WriteLine(" |        |         ");
         Console.WriteLine(" |        O         ");
         Console.WriteLine(" |       /|\\     ");
-        Console.WriteLine(" |       / \\     "); //ja tem o resto dos membros ^_^
+        Console.WriteLine(" |       / \\     "); 
         Console.WriteLine("");
         Console.WriteLine("");
         Console.WriteLine("");
